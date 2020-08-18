1b78ba06dc1132020d4b5d5131315b3c97a226e1
nl96 <68866318+nl96@users.noreply.github.com>
2020-08-18 22:06:09 +0300

    Login added

--- init.js
+++ init.js
@@ -1,4 +1,5 @@
 const LIST_URL_FRUTALES = "https://nl96.github.io/vivero/frutales.json";
+const USUARIOS_URL = "https://danielk2020.github.io/biblioteca/usuarios.json";
 
 var getJSONData = function(url){
     var result = {};
@@ -21,3 +22,22 @@ var getJSONData = function(url){
         return result;
     });
 }
+
+
+document.addEventListener("DOMContentLoaded", function (e) {
+  let userLogged = localStorage.getItem("User-Logged");
+  let infoUser = document.getElementById("login");
+  let user = document.getElementById("user");
+
+  if (userLogged) {
+    infoUser.style.display = "inline-block";
+    userLogged = JSON.parse(userLogged);
+    user.innerText = user.innerText + "Usuario logeado: " + userLogged.email;
+  }
+
+  document.getElementById("logout").addEventListener("click", function(){
+    localStorage.removeItem("User-Logged");
+    window.location = "login.html";
+  });
+  
+});
---
+++ login.js
@@ -0,0 +1,51 @@
+let usersArray = [];
+
+function validateUser(array, userIn, passwordIn) {
+    for (let i = 0; i < array.length; i++) {
+        let usuario = array[i];
+        if (usuario.email == userIn && usuario.password == passwordIn) {
+            return true;
+        }
+    }
+    return false;
+}
+
+document.addEventListener("DOMContentLoaded", function (e) {
+    document.getElementById("submit").addEventListener("click", function(e){
+        let inputEmail = document.getElementById("inputEmail");
+        let inputPassword = document.getElementById("inputPassword");
+        let camposCompletos = true;
+    
+        if (inputEmail.value === ""){
+            inputEmail.classList.add("invalid");
+            camposCompletos = false;
+        }
+        if (inputPassword.value === ""){
+            inputPassword.classList.add("invalid");
+            camposCompletos = false;
+        }
+    
+        if (camposCompletos){
+            
+            getJSONData(USUARIOS_URL).then(function (resultObj){
+                if (resultObj.status === "ok"){
+                    usersArray = resultObj.data;
+        
+                    if (validateUser(usersArray, inputEmail.value, inputPassword.value)) {
+
+                        localStorage.setItem("User-Logged", JSON.stringify({email: inputEmail.value}));
+                        
+                        window.location = "index.html";
+                    } else {
+                        alert("Usuario o contraseña incorrectos");
+                    }
+                }
+            });
+
+        }
+        else{
+            alert("Debes ingresar correctamente los datos");
+        }
+
+    });
+});
