2abc474c500f1e51c971d892f587d34c1d9695e7
nl96 <68866318+nl96@users.noreply.github.com>
2020-08-18 22:05:17 +0300

    Filtro de precio añadido

--- frutales.js
+++ frutales.js
@@ -1,4 +1,7 @@
-var frutalesArray = [];
+let frutalesArray = [];
+
+let minPrice = undefined;
+let maxPrice = undefined;
 
 function showItemsList(array){
 
@@ -6,18 +9,23 @@ function showItemsList(array){
     for(let i = 0; i < array.length; i++){
         let pos = array[i];
 
-        item += `
-        <div class="item">
-            <h2 class="">`+ pos.title +`</h2>
-            <span class="float-right-top" title="` + pos.currency + `"> Precio: $` + pos.price + `</span>
-            <p>` + pos.description + `</p>
-        </div>`
+        if ((minPrice == undefined || (minPrice != undefined && minPrice <= parseInt(pos.price))) && 
+            (maxPrice == undefined || (maxPrice != undefined && maxPrice >= parseInt(pos.price)))) {
+
+            item += `
+            <div class="item">
+                <h2 class="">`+ pos.title +`</h2>
+                <span class="float-right-top" title="` + pos.currency + `"> Precio: $` + pos.price + `</span>
+                <p>` + pos.description + `</p>
+            </div>`
+        }
 
         document.getElementById("listado").innerHTML = item;
     }
 }
 
 document.addEventListener("DOMContentLoaded", function(e){
+
     getJSONData(LIST_URL_FRUTALES).then(function(resultObj){
         if (resultObj.status === "ok")
         {
@@ -26,4 +34,38 @@ document.addEventListener("DOMContentLoaded", function(e){
             showItemsList(frutalesArray);
         }
     });
+
+    document.getElementById("filter").addEventListener("click", function() {
+        minPrice = document.getElementById("min-value").value;
+        maxPrice = document.getElementById("max-value").value;
+
+        if (minPrice != undefined && minPrice != "" && parseInt(minPrice) >= 0) {
+            minPrice = parseInt(minPrice);
+        }
+        else {
+            minPrice = undefined;
+        }
+        if (maxPrice != undefined && maxPrice != "" && parseInt(maxPrice) >= 0) {
+            maxPrice = parseInt(maxPrice);
+        }
+        else {
+            maxPrice = undefined;
+        }
+
+        //Muestro los productos filtrados
+        showItemsList(frutalesArray);
+
+    });
+
+    document.getElementById("erase").addEventListener("click", function() {
+        document.getElementById("min-value").value = undefined;
+        document.getElementById("max-value").value = undefined;
+
+        minPrice = undefined;
+        maxPrice = undefined;
+
+        //Muestro todos los productos
+        showItemsList(frutalesArray);
+    });
+
 });
